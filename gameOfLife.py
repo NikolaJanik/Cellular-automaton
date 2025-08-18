f249e702289a774d0d89e280683f16ea9f6b741e
Nikola Janik <131357328+NikolaJanik@users.noreply.github.com>
2025-08-18 19:14:48 +0200

    Update gameOfLife.py

--- gameOfLife.py
+++ gameOfLife.py
@@ -1,6 +1,50 @@
 import numpy as np
 import time
 import pygame
+import button
+
+class Menu:
+    def __init__(self, win_size, surface):
+        self.surface = surface
+        self.color = (0, 0, 0)
+        self.menu_mode = True
+
+        imgClearGrid = pygame.image.load("clearGrid.png").convert_alpha()
+        imgBomber = pygame.image.load("bomber.png").convert_alpha()
+        imgCopperhead = pygame.image.load("copperhead.png").convert_alpha()
+        imgDart = pygame.image.load("dart.png").convert_alpha()
+        imgDiamond = pygame.image.load("diamond.png").convert_alpha()
+        imgGGG = pygame.image.load("gosperGlinderGun.png").convert_alpha()
+        imgKickback = pygame.image.load("kickback.png").convert_alpha()
+        imgLoafer = pygame.image.load("loafer.png").convert_alpha()
+        imgPenta = pygame.image.load("pentadecathlon.png").convert_alpha()
+
+        self.buttonClearGrid = button.Button(0, 0, imgClearGrid, 2)
+        self.buttonBomber = button.Button(0, 50, imgBomber, 1)
+        self.buttonCopperhead = button.Button(0, 0, imgCopperhead, 1)
+        self.buttonDart = button.Button(0, 50, imgDart, 1)
+        self.buttonDiamond = button.Button(0, 0, imgDiamond, 1)
+        self.buttonGGG = button.Button(0, 50, imgGGG, 1)
+        self.buttonKickback = button.Button(0, 0, imgKickback, 1)
+        self.buttonLoafer = button.Button(0, 50, imgLoafer, 1)
+        self.buttonPenta = button.Button(0, 0, imgPenta, 1)
+
+    def events(self):
+        self.surface.fill(self.color)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                quit()
+
+        if self.buttonClearGrid.draw(self.surface):
+            self.menu_mode = False
+            self.surface.fill(self.color)
+        elif self.buttonBomber.draw(self.surface):
+            self.menu_mode = False
+            self.surface.fill(self.color)
+        pygame.display.update()
+
 
 COLOR_BG = (10,10,10)
 COLOR_GRID = (40,40,40)
